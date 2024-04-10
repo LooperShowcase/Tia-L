dae7552f86fc42c76d325a1aff8f76c9d089e521
Looper Project Showcases <87170787+LooperShowcase@users.noreply.github.com>
2024-04-10 13:28:34 +0300

    Add files via upload

---
+++ Ti.py
@@ -0,0 +1,30 @@
+def my_sum(n1,n2,n3,n4,n5):
+    return n1+n2+n3+n4+n5
+
+def my_avg(x,y,z,w,e):
+    return (my_sum(100,20,200,90,2)/5)
+
+print("The average is " , my_avg(100,90,50,10,2))
+my_avg(100,20,90,10,100)
+
+
+
+
+# def My_Sum(n1,n2,n3,n4,n5):
+#     print(n1+n2+n3+n4+n5)
+# My_Sum(1,8,6,10,7)
+
+# def My_Prod(n6,n7,n8,n9,n10):
+#     print(n6*n7*n7*n8*n9*n10)
+# My_Prod(5,10,20,2,1)
+
+
+
+# def Fizz_B(num):
+#     if num / 3 :
+#         print("Fizz")
+
+#     elif num / 5 :
+#         print("Bazz")
+    
+#     else 
---
+++ script.py
@@ -0,0 +1,5 @@
+def myavg(n1,n2,n3,n4) :
+    x = (n1 + n2 + n3 + n4 )/4
+    print("a+b= " , x)
+
+myavg(10,20,50,0)
